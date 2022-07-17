a710f214843e1da4cc5629ab759512dc148d4ae2
ali_s <ali_s@DRAGON>
2022-07-17 16:14:58 +0430

    Add project files.

---
+++ Shortest Route To Self/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿namespace Shortest_Route_To_Self
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label_ansShort = new System.Windows.Forms.Label();
+            this.btn_calculate = new System.Windows.Forms.Button();
+            this.btn_loadFile = new System.Windows.Forms.Button();
+            this.txtBox_filePath = new System.Windows.Forms.TextBox();
+            this.dataGridViewCsv = new System.Windows.Forms.DataGridView();
+            this.lbl_filePath = new System.Windows.Forms.Label();
+            this.label_ansLong = new System.Windows.Forms.Label();
+            this.label_routeShort = new System.Windows.Forms.Label();
+            this.label_routeLong = new System.Windows.Forms.Label();
+            this.label_info = new System.Windows.Forms.Label();
+            this.label_note = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCsv)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label_ansShort
+            // 
+            this.label_ansShort.AutoSize = true;
+            this.label_ansShort.Location = new System.Drawing.Point(37, 412);
+            this.label_ansShort.Name = "label_ansShort";
+            this.label_ansShort.Size = new System.Drawing.Size(65, 13);
+            this.label_ansShort.TabIndex = 0;
+            this.label_ansShort.Text = "Lowest Cost";
+            this.label_ansShort.Visible = false;
+            // 
+            // btn_calculate
+            // 
+            this.btn_calculate.Location = new System.Drawing.Point(294, 383);
+            this.btn_calculate.Name = "btn_calculate";
+            this.btn_calculate.Size = new System.Drawing.Size(75, 23);
+            this.btn_calculate.TabIndex = 1;
+            this.btn_calculate.Text = "Calculate";
+            this.btn_calculate.UseVisualStyleBackColor = true;
+            this.btn_calculate.Visible = false;
+            this.btn_calculate.Click += new System.EventHandler(this.btn_calculate_Click);
+            // 
+            // btn_loadFile
+            // 
+            this.btn_loadFile.Location = new System.Drawing.Point(551, 54);
+            this.btn_loadFile.Name = "btn_loadFile";
+            this.btn_loadFile.Size = new System.Drawing.Size(75, 23);
+            this.btn_loadFile.TabIndex = 2;
+            this.btn_loadFile.Text = "Choose File";
+            this.btn_loadFile.UseVisualStyleBackColor = true;
+            this.btn_loadFile.Click += new System.EventHandler(this.btn_loadFile_Click);
+            // 
+            // txtBox_filePath
+            // 
+            this.txtBox_filePath.Location = new System.Drawing.Point(15, 56);
+            this.txtBox_filePath.Name = "txtBox_filePath";
+            this.txtBox_filePath.Size = new System.Drawing.Size(415, 20);
+            this.txtBox_filePath.TabIndex = 3;
+            // 
+            // dataGridViewCsv
+            // 
+            this.dataGridViewCsv.BackgroundColor = System.Drawing.Color.White;
+            this.dataGridViewCsv.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            this.dataGridViewCsv.Location = new System.Drawing.Point(12, 83);
+            this.dataGridViewCsv.Name = "dataGridViewCsv";
+            this.dataGridViewCsv.Size = new System.Drawing.Size(626, 254);
+            this.dataGridViewCsv.TabIndex = 4;
+            // 
+            // lbl_filePath
+            // 
+            this.lbl_filePath.AutoSize = true;
+            this.lbl_filePath.Location = new System.Drawing.Point(28, 41);
+            this.lbl_filePath.Name = "lbl_filePath";
+            this.lbl_filePath.Size = new System.Drawing.Size(181, 13);
+            this.lbl_filePath.TabIndex = 6;
+            this.lbl_filePath.Text = "CSV File Path (Ex:  c:\\temp\\test.csv)";
+            // 
+            // label_ansLong
+            // 
+            this.label_ansLong.AutoSize = true;
+            this.label_ansLong.Location = new System.Drawing.Point(395, 412);
+            this.label_ansLong.Name = "label_ansLong";
+            this.label_ansLong.Size = new System.Drawing.Size(67, 13);
+            this.label_ansLong.TabIndex = 7;
+            this.label_ansLong.Text = "Highest Cost";
+            this.label_ansLong.Visible = false;
+            // 
+            // label_routeShort
+            // 
+            this.label_routeShort.AutoSize = true;
+            this.label_routeShort.Location = new System.Drawing.Point(37, 383);
+            this.label_routeShort.Name = "label_routeShort";
+            this.label_routeShort.Size = new System.Drawing.Size(35, 13);
+            this.label_routeShort.TabIndex = 8;
+            this.label_routeShort.Text = "label1";
+            this.label_routeShort.Visible = false;
+            // 
+            // label_routeLong
+            // 
+            this.label_routeLong.AutoSize = true;
+            this.label_routeLong.Location = new System.Drawing.Point(395, 383);
+            this.label_routeLong.Name = "label_routeLong";
+            this.label_routeLong.Size = new System.Drawing.Size(35, 13);
+            this.label_routeLong.TabIndex = 9;
+            this.label_routeLong.Text = "label1";
+            this.label_routeLong.Visible = false;
+            // 
+            // label_info
+            // 
+            this.label_info.AutoSize = true;
+            this.label_info.Location = new System.Drawing.Point(12, 9);
+            this.label_info.Name = "label_info";
+            this.label_info.Size = new System.Drawing.Size(627, 13);
+            this.label_info.TabIndex = 10;
+            this.label_info.Text = "This App determines Lowest and Highest Cost of circuits of node \'A\' in a Weighted" +
+    " Complete Graph where the Pure Cost is positive. ";
+            // 
+            // label_note
+            // 
+            this.label_note.AutoSize = true;
+            this.label_note.Location = new System.Drawing.Point(15, 344);
+            this.label_note.Name = "label_note";
+            this.label_note.Size = new System.Drawing.Size(478, 13);
+            this.label_note.TabIndex = 11;
+            this.label_note.Text = "Note: A Node can only be traversed Once and Two Nodes must be traversed before re" +
+    "turning to \'A\'.";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(653, 452);
+            this.Controls.Add(this.label_note);
+            this.Controls.Add(this.label_info);
+            this.Controls.Add(this.label_routeLong);
+            this.Controls.Add(this.label_routeShort);
+            this.Controls.Add(this.label_ansLong);
+            this.Controls.Add(this.lbl_filePath);
+            this.Controls.Add(this.dataGridViewCsv);
+            this.Controls.Add(this.txtBox_filePath);
+            this.Controls.Add(this.btn_loadFile);
+            this.Controls.Add(this.btn_calculate);
+            this.Controls.Add(this.label_ansShort);
+            this.Name = "Form1";
+            this.Text = "Cost of Circuit of A";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridViewCsv)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label_ansShort;
+        private System.Windows.Forms.Button btn_calculate;
+        private System.Windows.Forms.Button btn_loadFile;
+        private System.Windows.Forms.TextBox txtBox_filePath;
+        private System.Windows.Forms.DataGridView dataGridViewCsv;
+        private System.Windows.Forms.Label lbl_filePath;
+        private System.Windows.Forms.Label label_ansLong;
+        private System.Windows.Forms.Label label_routeShort;
+        private System.Windows.Forms.Label label_routeLong;
+        private System.Windows.Forms.Label label_info;
+        private System.Windows.Forms.Label label_note;
+    }
+}
+
---
+++ Shortest Route To Self/Form1.cs
@@ -0,0 +1,230 @@
+﻿using Microsoft.VisualBasic.FileIO;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Shortest_Route_To_Self
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private static readonly List<string[]> routes = new List<string[]>();
+        private static readonly List<string> steps = new List<string>();
+        private static readonly List<string[]> csvFile = new List<string[]>();
+
+        private static bool CheckListEqual(string[][] a,string[] b)
+        {
+            for (int i = 0; i < a.Length; i++)
+            {
+                int counter = 0;
+                for (int j = 0; j < b.Length; j++)
+                {
+                    if (a[i][j] == b[j]) 
+                        counter++;
+                    if (counter == b.Length)
+                        return true;
+                    
+                }
+            }
+            return false;
+        }
+
+        static void addToList(string[] a, int size)
+        {
+            for (int i = 0; i < size; i++)
+                steps.Add(a[i]);
+
+            string[] t = steps.ToArray();
+            string[][] s = routes.ToArray();
+            if (!CheckListEqual(s,t))
+            {
+                routes.Add(steps.ToArray());
+            } 
+            steps.Clear();
+        }
+
+        static void HeapPermutation(string[] a, int n, int size)
+        {
+            if (n == 1)
+                addToList(a, size);
+
+            for (int i = 0; i < n; i++)
+            {
+                HeapPermutation(a, n - 1, size);
+                if (n % 2 == 1)
+                {
+                    string temp = a[0];
+                    a[0] = a[n - 1];
+                    a[n - 1] = temp;
+                }
+                else
+                {
+                    string temp = a[i];
+                    a[i] = a[n - 1];
+                    a[n - 1] = temp;
+                }
+            }
+        }
+
+
+        private void btn_calculate_Click(object sender, EventArgs e)
+        {
+            btn_calculate.Visible = false;
+            string[][] input = csvFile.ToArray();
+            int inputRow = input.GetLength(0);
+            List<string> nodes = new List<string>();
+            for (int i = 0; i < inputRow; i++)
+            {
+                if (!nodes.Contains(input[i][0]))
+                {
+                    nodes.Add(input[i][0]);
+                }
+                if (!nodes.Contains(input[i][1]))
+                {
+                    nodes.Add((input[i][1]));
+                }
+            }
+
+            nodes.Remove("A");
+            string[] finalNodes = nodes.ToArray();
+            List<string[]> allRoutes = new List<string[]>();
+
+            for (int i = 2;i <= finalNodes.Length; i++)
+            {
+                HeapPermutation(finalNodes, finalNodes.Length, i);
+                allRoutes.AddRange(routes);
+                routes.Clear();
+            }
+
+            string[][] finalRoutes = allRoutes.ToArray();
+            float[] cost = new float[finalRoutes.Length];
+            float[] pureCost = new float[finalRoutes.Length];           
+            string check1 = "A";
+            string check2;
+
+            for (int i = 0;i < finalRoutes.Length; i++)
+            {
+                for (int j = 0;j <= finalRoutes[i].Length; j++)
+                {
+                    if (j == finalRoutes[i].Length)
+                    {
+                        check2 = "A";
+                    }
+                    else check2 = finalRoutes[i][j];
+
+                    for (int t = 0; t < inputRow; t++)
+                    {
+                        if ((input[t][0] == check1 && input[t][1] == check2) || (input[t][0] == check2 && input[t][1] == check1))
+                        {
+                            cost[i] += float.Parse(input[t][2], CultureInfo.InvariantCulture);
+                            pureCost[i] += float.Parse(input[t][3], CultureInfo.InvariantCulture);
+                            check1 = check2;
+                        }
+
+                    }
+                }
+                
+            }
+            while (true)
+            {
+                float minCost = cost.AsQueryable().Min();
+                int indexMin = Array.IndexOf(cost, minCost);
+                if (pureCost[indexMin] < 0)
+                {
+                    cost = cost.Where((source, index) => index != indexMin).ToArray();
+                    pureCost = pureCost.Where((source, index) => index != indexMin).ToArray();
+                    finalRoutes = finalRoutes.Where((source, index) => index != indexMin).ToArray();
+                }
+                else
+                {
+                    string route = "Route: A => ";
+                    foreach (string step in finalRoutes[indexMin])
+                    {
+                        route = route + step + " => ";
+                    }
+                    route = route + "A";
+                    label_routeShort.Text = route;
+                    label_routeShort.Visible = true;
+                    label_ansShort.Text = "The Lowest Cost is: " + minCost;
+                    label_ansShort.Visible = true;
+                    break;
+                }
+                    
+            }
+            while (true)
+            {
+                float maxCost = cost.AsQueryable().Max();
+                int indexMax = Array.IndexOf(cost, maxCost);
+                if (pureCost[indexMax] < 0)
+                {
+                    cost = cost.Where((source, index) => index != indexMax).ToArray();
+                    pureCost = pureCost.Where((source, index) => index != indexMax).ToArray();
+                    finalRoutes = finalRoutes.Where((source, index) => index != indexMax).ToArray();
+                }
+                else
+                {
+                    string route = "Route: A => ";
+                    foreach (string step in finalRoutes[indexMax])
+                    {
+                        route = route + step + " => ";
+                    }
+                    route = route + "A";
+                    label_routeLong.Text = route;
+                    label_routeLong.Visible = true;
+                    label_ansLong.Text = "The Highest Cost is: " + maxCost;
+                    label_ansLong.Visible = true;
+                    break;
+                }
+            }
+        }
+
+        private void btn_loadFile_Click(object sender, EventArgs e)
+        {
+            csvFile.Clear();
+            routes.Clear();
+            steps.Clear();
+            label_ansLong.Visible=false;
+            label_ansShort.Visible=false;
+            label_routeLong.Visible=false;
+            label_routeShort.Visible=false;
+            string filePath = txtBox_filePath.Text;
+            using (TextFieldParser parser = new TextFieldParser(@filePath))
+            {
+                parser.TextFieldType = FieldType.Delimited;
+                parser.SetDelimiters(",");
+                DataTable dt = new DataTable();
+                dt.Columns.Add(new DataColumn("First Node"));
+                dt.Columns.Add(new DataColumn("Second Node"));
+                dt.Columns.Add(new DataColumn("Cost"));
+                dt.Columns.Add(new DataColumn("Pure Cost"));
+
+                while (!parser.EndOfData)
+                {
+                    string[] fields = parser.ReadFields();
+                    csvFile.Add(fields);
+                    dt.Rows.Add();
+                    int i = 0;
+                    foreach (string cell in fields)
+                    {
+                        dt.Rows[dt.Rows.Count - 1][i] = cell;
+                        i++;
+                    }
+                }
+                dataGridViewCsv.DataSource = dt;
+                btn_calculate.Visible = true;
+            }
+        }
+    }
+}
+
